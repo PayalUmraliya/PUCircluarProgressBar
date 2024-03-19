b422ead08a6f2f18effbab4645bd245e6c12151b
Payal Makwana <payal.makwana-v@adityabirlacapital.com>
2024-03-19 14:53:38 +0530

    Circular slider code added

--- PUWhatsAppStatusProgressBar/PUWAppStatusProgressIndicator.swift
+++ PUWhatsAppStatusProgressBar/PUWAppStatusProgressIndicator.swift
@@ -10,7 +10,246 @@ import UIKit
 
 typealias Degrees = Double
 typealias Radians = CGFloat
+extension Date
+{
+    func getDaysInMonth() -> Int
+    {
+            let calendar = Calendar.current
+            let dateComponents = DateComponents(year: calendar.component(.year, from: self), month: calendar.component(.month, from: self))
+            let date = calendar.date(from: dateComponents)!
+            let range = calendar.range(of: .day, in: .month, for: date)!
+            let numDays = range.count
+            return numDays
+    }
+}
+// Circular Slider
 
+struct PUCircularSliderSettings {
+    
+     var minimumValue: Float = 0
+     var maximumValue: Float = 500
+    var lineWidth:CGFloat = 5
+    var ringColor: UIColor = UIColor.lightGray
+    var circleFillColor:UIColor = UIColor.clear
+    var puNormalColor: UIColor = UIColor.darkGray
+    var puHighlightedColor: UIColor = UIColor.green
+    var highlighted: Bool = true
+    var ThumbIndicatorRadius: CGFloat = 20
+    var radiansOffset: CGFloat = 0
+    var isPlainColor:Bool = false
+    var gradientColors:[UIColor] = [.yellow,.orange,.white]
+}
+
+open class PUCircularSlider: UIView {
+
+    fileprivate var backgroundRingLayer = CAShapeLayer()
+    fileprivate var progressRingLayer = CAShapeLayer()
+    fileprivate var thumbIndicatorLayer = CAShapeLayer()
+    fileprivate var defaultValue: Float = 0
+    fileprivate var defaultThumbIndicatorAngle: CGFloat = 0
+    fileprivate var startAngle: CGFloat {
+        return -CGFloat.pi / 2 + settings.radiansOffset
+    }
+    fileprivate var endAngle: CGFloat {
+        return 3 * CGFloat(Double.pi / 2) - settings.radiansOffset
+    }
+    fileprivate var angleRange: CGFloat {
+        return endAngle - startAngle
+    }
+    fileprivate var valueRange: Float {
+        return settings.maximumValue - settings.minimumValue
+    }
+    fileprivate var arcCenter: CGPoint {
+        return CGPoint(x: frame.width / 2, y: frame.height / 2)
+    }
+    fileprivate var arcRadius: CGFloat {
+        return min(frame.width,frame.height) / 2 - settings.lineWidth / 2
+    }
+    fileprivate var calculatedValue: Float {
+        return (value - settings.minimumValue) / (settings.maximumValue - settings.minimumValue)
+    }
+    fileprivate var ThumbIndicatorAngle: CGFloat {
+        return CGFloat(calculatedValue) * angleRange + startAngle
+    }
+    fileprivate var ThumbIndicatorMidAngle: CGFloat {
+        let angleRange = (startAngle - endAngle).truncatingRemainder(dividingBy: 2 * .pi)
+        let normalizedEndAngle = (endAngle + angleRange).truncatingRemainder(dividingBy: 2 * .pi)
+        return (2 * .pi + normalizedEndAngle) / 2
+    }
+
+    fileprivate var ThumbIndicatorRotationTransform: CATransform3D {
+        return CATransform3DMakeRotation(ThumbIndicatorAngle, 0.0, 0.0, 1)
+    }
+    private let gradientLayer: CAGradientLayer = {
+           let gradientLayer = CAGradientLayer()
+           gradientLayer.type = .conic
+        gradientLayer.startPoint = CGPoint(x: 0.8, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
+           return gradientLayer
+       }()
+    var settings: PUCircularSliderSettings = PUCircularSliderSettings(){
+        didSet {
+            settingUp()
+            setNeedsDisplay()
+        }
+    }
+    open var value: Float {
+        get {
+            return defaultValue
+        }
+        set {
+            defaultValue = min(settings.maximumValue, max(settings.minimumValue, newValue))
+        }
+    }
+ 
+    
+    // MARK: - init
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        settingUp()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        settingUp()
+    }
+
+    override open func draw(_ rect: CGRect) {
+        print("drawRect")
+        backgroundRingLayer.bounds = bounds
+        progressRingLayer.bounds = bounds
+        thumbIndicatorLayer.bounds = bounds
+        backgroundRingLayer.position = arcCenter
+        progressRingLayer.position = arcCenter
+        thumbIndicatorLayer.position = arcCenter
+        backgroundRingLayer.path = getCirclePath()
+        progressRingLayer.path = getCirclePath()
+        thumbIndicatorLayer.path = getThumbIndicatorPath()
+        setValue(value, animated: false)
+    }
+    
+    
+    fileprivate func getCirclePath() -> CGPath {
+        return UIBezierPath(arcCenter: arcCenter,
+                            radius: arcRadius,
+                            startAngle: startAngle,
+                            endAngle: endAngle,
+                            clockwise: true).cgPath
+    }
+    
+    fileprivate func getThumbIndicatorPath() -> CGPath {
+        return UIBezierPath(roundedRect:
+                                CGRect(x: arcCenter.x + arcRadius - settings.ThumbIndicatorRadius / 2, y: arcCenter.y - settings.ThumbIndicatorRadius / 2, width: settings.ThumbIndicatorRadius, height: settings.ThumbIndicatorRadius),
+                            cornerRadius: settings.ThumbIndicatorRadius / 2).cgPath
+    }
+    
+    
+    // MARK: - settingUp
+    fileprivate func settingUp() {
+        clipsToBounds = false
+        self.value = 0
+        settingUpBackgroundLayer()
+        settingUpProgressLayer()
+        settingUpthumbIndicatorLayer()
+    }
+    
+    fileprivate func settingUpBackgroundLayer() {
+        backgroundRingLayer.frame = bounds
+        layer.addSublayer(backgroundRingLayer)
+        groomBackgroundLayer()
+    }
+    
+    fileprivate func settingUpProgressLayer() {
+        progressRingLayer.frame = bounds
+        progressRingLayer.strokeEnd = 0
+        layer.addSublayer(progressRingLayer)
+        groomProgressLayer()
+    }
+    
+    fileprivate func settingUpthumbIndicatorLayer() {
+        thumbIndicatorLayer.frame = bounds
+        thumbIndicatorLayer.position = arcCenter
+        thumbIndicatorLayer.path = self.getThumbIndicatorPath()
+        thumbIndicatorLayer.fillColor = UIColor.blue.cgColor
+        layer.addSublayer(thumbIndicatorLayer)
+        groomThumbIndicatorLayer()
+    }
+    
+    fileprivate func groomBackgroundLayer() {
+        backgroundRingLayer.lineWidth = settings.lineWidth
+        backgroundRingLayer.fillColor = settings.circleFillColor.cgColor
+        backgroundRingLayer.strokeColor = settings.ringColor.cgColor
+        backgroundRingLayer.lineCap = .round
+    }
+    
+    fileprivate func groomProgressLayer() {
+        progressRingLayer.lineWidth = settings.lineWidth
+        progressRingLayer.fillColor = UIColor.clear.cgColor
+        progressRingLayer.strokeColor = settings.highlighted ? settings.puHighlightedColor.cgColor : settings.puNormalColor.cgColor
+        progressRingLayer.lineCap = .round
+        self.setUpGradientLayer(maskedLayer: progressRingLayer )
+    }
+    
+    fileprivate func groomThumbIndicatorLayer() {
+        thumbIndicatorLayer.lineWidth = 2
+        thumbIndicatorLayer.fillColor = settings.highlighted ? settings.puHighlightedColor.cgColor : settings.puNormalColor.cgColor
+        thumbIndicatorLayer.strokeColor = UIColor.white.cgColor
+       
+    }
+    func setUpGradientLayer(maskedLayer:CAShapeLayer){
+            gradientLayer.frame = bounds
+            gradientLayer.colors = settings.gradientColors.map { $0.cgColor }
+            layer.addSublayer(gradientLayer)
+            gradientLayer.mask = maskedLayer
+    }
+    // MARK: - update
+    open func setValue(_ value: Float, animated: Bool) {
+        self.value =  value
+        setStrokeEnd(animated: animated)
+        setThumbIndicatorRotation(animated: animated)
+    }
+    
+    fileprivate func setStrokeEnd(animated: Bool) {
+        
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        
+        let strokeAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        strokeAnimation.duration = animated ? 0.66 : 0
+        strokeAnimation.repeatCount = 1
+        strokeAnimation.fromValue = progressRingLayer.strokeEnd
+        strokeAnimation.toValue = CGFloat(calculatedValue)
+        strokeAnimation.isRemovedOnCompletion = false
+        strokeAnimation.fillMode = CAMediaTimingFillMode.removed
+        strokeAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        progressRingLayer.add(strokeAnimation, forKey: "strokeAnimation")
+        progressRingLayer.strokeEnd = CGFloat(calculatedValue)
+        CATransaction.commit()
+    }
+    
+    fileprivate func setThumbIndicatorRotation(animated: Bool) {
+        CATransaction.begin()
+        CATransaction.setDisableActions(true)
+        
+        let animation = CAKeyframeAnimation(keyPath: "transform.rotation.z")
+        animation.duration = animated ? 0.66 : 0
+        animation.values = [defaultThumbIndicatorAngle, ThumbIndicatorAngle]
+        thumbIndicatorLayer.add(animation, forKey: "ThumbIndicatorRotationAnimation")
+        thumbIndicatorLayer.transform = ThumbIndicatorRotationTransform
+        
+        CATransaction.commit()
+        
+        defaultThumbIndicatorAngle = ThumbIndicatorAngle
+    }
+    
+    func cancelAnimation() {
+        progressRingLayer.removeAllAnimations()
+        thumbIndicatorLayer.removeAllAnimations()
+    }
+    
+}
+
+//WHATSAPP STORY UI
 struct PUWAppStatusProgressIndicatorSettings {
     
     var segmentsCount: Int = 4
@@ -82,7 +321,7 @@ class PUWAppStatusProgressIndicator: UIView {
     }
     func decideSegemntColorOfIdx(idx:Int) -> UIColor
     {
-        if idx < settings.colorSegementCount && idx != settings.targetSegementNumber 
+        if idx < settings.colorSegementCount && idx != settings.targetSegementNumber
         {
             return settings.segmentColor
         }
@@ -186,7 +425,7 @@ class PUWAppStatusProgressIndicator: UIView {
         segmentLayer.strokeStart = 0.0
         return segmentLayer
     }
-    
+
     private func updateProgressInLayer(progressLayer: CAShapeLayer,
                                        percent: CGFloat,
                                        segmentIndex: Int) {
@@ -259,19 +498,4 @@ class PUWAppStatusProgressIndicator: UIView {
         
     }
     
-}
-extension Date {
-
-func getDaysInMonth() -> Int{
-    let calendar = Calendar.current
-
-    let dateComponents = DateComponents(year: calendar.component(.year, from: self), month: calendar.component(.month, from: self))
-    let date = calendar.date(from: dateComponents)!
-
-    let range = calendar.range(of: .day, in: .month, for: date)!
-    let numDays = range.count
-
-    return numDays
-}
-
 }
--- PUWhatsAppStatusProgressBar/ViewController.swift
+++ PUWhatsAppStatusProgressBar/ViewController.swift
@@ -8,12 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+ 
     @IBOutlet weak var vwProgress: UIView!
     var indicators: [PUWAppStatusProgressIndicator] = []
     override func viewDidLoad() {
         super.viewDidLoad()
+       
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
             self.setupADStausProgressBar()
+           
             var progressInPercents = 1.0
             Timer.scheduledTimer(withTimeInterval: 0.2, repeats: true) { timer in
                 progressInPercents = progressInPercents + 2.0
@@ -26,6 +29,9 @@ class ViewController: UIViewController {
                 }
             }
         }
+        
+            self.setupCircleIndicator()
+        
     }
 }
 
@@ -39,15 +45,36 @@ extension ViewController
         settings.defaultSegmentColor = UIColor.darkGray
         settings.segmentColor = UIColor.blue
         settings.targetSegmentColor = UIColor.yellow
-        settings.targetSegementNumber = 12 // total 13 Activedays allocated so value = total - 1
+        settings.targetSegementNumber = 12 // total 13 DAYS TARGET allocated so value = total - 1
         settings.segmentBorderType = .round
         settings.segmentsCount = Date().getDaysInMonth()
-        settings.colorSegementCount = 11 // TOTAL ACTIVE DAYS COUNT SET HERE
-        settings.spaceBetweenSegments = 1
-        settings.segmentWidth = 4
+        settings.colorSegementCount = 12 // your actual days count here
+        settings.spaceBetweenSegments = 2
+        settings.segmentWidth = 3
         let segment = PUWAppStatusProgressIndicator(frame: CGRect(x: 40, y: 100, width: 150.0, height: 150.0))
         segment.settings = settings
         indicators.append(segment)
         self.vwProgress.addSubview(segment)
     }
+    
+    func setupCircleIndicator() {
+        var settings = PUCircularSliderSettings()
+        settings.minimumValue = 0
+        settings.maximumValue = 1000
+        settings.lineWidth = 5
+        settings.puNormalColor = UIColor.yellow
+        settings.puHighlightedColor = UIColor.blue
+        settings.isPlainColor = false
+        settings.gradientColors = [UIColor.red,UIColor.blue,UIColor.white]
+        settings.highlighted = true
+        settings.ringColor = .black
+        settings.circleFillColor = .clear
+        let segment = PUCircularSlider(frame: CGRect(x: 40, y: 300, width: 200.0, height: 200.0))
+        segment.settings = settings
+        self.vwProgress.addSubview(segment)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            segment.setValue(160, animated: true)
+        }
+    }
 }
+
